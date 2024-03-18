6e437c52e1a324bddaba47b925b6df65328157ce
dan <rhayssadandara98@gmail.com>
2024-03-18 03:44:39 -0300

    cap3, ex6 - Incompleto

--- exercicios_cap3/aula_3_exercicios.R
+++ exercicios_cap3/aula_3_exercicios.R
@@ -95,3 +95,14 @@ view(populacao)
 
 pop_quilombola_total <- sum(base_pop_quilombola$pop_quilombola)
 pop_brasil_total <- sum(populacao$pop_total)
+
+
+################################
+#                              #
+# 6. Cruzando bases de dados I #
+#                              #
+################################
+
+# Use a variável cod_ibge para cruzar as duas bases de forma que a base resultante contenha informações sobre a população total e a população quilombola de cada município. Salve o resultado dessa operação no objeto municipios.
+
+municipios <- 
