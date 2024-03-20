8179d4cc4f2eae09c6179a4f76642c4e0082de65
dannog <dandaro.nogueira@gmail.com>
2024-03-19 21:15:50 -0300

    ex1, 2, 3 e 4 do cap4 - Finalizados

--- exercicios_cap4/aula_4_exemplos.R
+++ exercicios_cap4/aula_4_exemplos.R
@@ -102,5 +102,132 @@ ggplot(eseb, aes(x = D01A_FX_ID)) +
        x = "Faixa de Idade",
        y = "N") 
 
-# Coordenadas
+## Coordenadas
 
+
+pop_uf <- read_delim("POP2022_Brasil.csv", delim = ";")
+
+# podemos fazer um gráfico de barras que mostra a população de cada Unidade da Federação do Brasil com uma camada nova, coord_flip(), que inverte os eixos X e Y do gráfico:
+
+ggplot(pop_uf, aes(x = Unidades_Federacao, y=  POPULACAO)) + 
+  geom_col() +
+  labs(title = "População por UF",
+       subtitle = "Censo 2022 (IBGE)", y = "N") +
+  coord_flip() 
+
+
+## Facetas
+
+# Imagine o seguinte problema: temos os dados de faixa etária da população do país em um gráfico de barras, mas queremos ver essa distribuição para cada uma das cinco regiões do país. Como fazer isso sem precisar criar cinco gráficos individualmente? Esse é o problema que as facetas resolvem.
+
+ggplot(eseb, aes(x = D01A_FX_ID)) + 
+  geom_bar() +
+  scale_x_discrete(labels = c("16 e 17 anos" = "16-17",
+                              "18 a 24 anos" = "18-24",
+                              "25 a 34 Anos" = "25-34", 
+                              "35 a 44 Anos" = "35-44",
+                              "45 a 54 Anos" = "45-54",
+                              "55 a 64 anos" = "55-64",
+                              "65 e mais" = "65+")) +  
+  facet_wrap(~ REG) +
+  labs(title = "Entrevistados por faixa de idade",
+       subtitle = "ESEB 2022",
+       x = "Faixa de Idade",
+       y = "N") 
+
+# por padrão, facet_wrap usa a mesma escala dos eixos X e Y para todos os painéis, mas é possível mudar isso com o argumento scales, que podem assumir os valores de "fixed", "free", "free_x" e "free_y". Para deixar o eixo Y de cada painel variar livremente, por exemplo, podemos fazer o seguinte:
+
+ggplot(eseb, aes(x = D01A_FX_ID)) + 
+  geom_bar() +
+  scale_x_discrete(labels = c("16 e 17 anos" = "16-17",
+                              "18 a 24 anos" = "18-24",
+                              "25 a 34 Anos" = "25-34", 
+                              "35 a 44 Anos" = "35-44",
+                              "45 a 54 Anos" = "45-54",
+                              "55 a 64 anos" = "55-64",
+                              "65 e mais" = "65+")) + 
+  facet_wrap(~ REG, scales = "free_y") +
+  labs(title = "Entrevistados por faixa de idade",
+       subtitle = "ESEB 2022",
+       x = "Faixa de Idade",
+       y = "N") 
+
+# outro argumento útil da função facet_wrap é ncol, que permite especificar o número de colunas que os painéis devem ter. 
+
+ggplot(eseb, aes(x = D01A_FX_ID)) + 
+  geom_bar() +
+  scale_x_discrete(labels = c("16 e 17 anos" = "16-17",
+                              "18 a 24 anos" = "18-24",
+                              "25 a 34 Anos" = "25-34", 
+                              "35 a 44 Anos" = "35-44",
+                              "45 a 54 Anos" = "45-54",
+                              "55 a 64 anos" = "55-64",
+                              "65 e mais" = "65+")) +  
+  facet_wrap(~ REG, ncol = 2) +
+  labs(title = "Entrevistados por faixa de idade",
+       subtitle = "ESEB 2022",
+       x = "Faixa de Idade",
+       y = "N") 
+
+#  Há outras funções facet_ que podem ser usadas para fazer gráficos em subgrupos do mesmo conjunto de dados: facet_grid, por exemplo, é usada para fazer gráficos em subgrupos de duas variáveis, uma para o eixo X e outra para o eixo Y. A função facet_grid é especialmente útil para fazer gráficos em subgrupos de duas variáveis categóricas, como no exemplo a seguir
+
+ggplot(eseb, aes(x = D01A_FX_ID)) + 
+  geom_bar() +
+  scale_x_discrete(labels = c("16 e 17 anos" = "16-17",
+                              "18 a 24 anos" = "18-24",
+                              "25 a 34 Anos" = "25-34", 
+                              "35 a 44 Anos" = "35-44",
+                              "45 a 54 Anos" = "45-54",
+                              "55 a 64 anos" = "55-64",
+                              "65 e mais" = "65+")) +  facet_grid(REG ~ D02) +
+  labs(title = "Entrevistados por faixa de idade",
+       subtitle = "ESEB 2022",
+       x = "Faixa de Idade",
+       y = "N") 
+
+
+## Temas
+
+# temas são funções que controlam vários recursos de exibição de uma visualização, como tamanho da fonte e cor do plano de fundo, rotação e cor de textos, grades e linhas de eixos, entre inúmeras outras coisas.
+
+ggplot(eseb, aes(x = D01A_FX_ID)) + 
+  geom_bar() +
+  scale_x_discrete(labels = c("16 e 17 anos" = "16-17",
+                              "18 a 24 anos" = "18-24",
+                              "25 a 34 Anos" = "25-34", 
+                              "35 a 44 Anos" = "35-44",
+                              "45 a 54 Anos" = "45-54",
+                              "55 a 64 anos" = "55-64",
+                              "65 e mais" = "65+")) +  facet_grid(REG ~ D02) +
+  labs(title = "Entrevistados por faixa de idade",
+       subtitle = "ESEB 2022",
+       x = "Faixa de Idade",
+       y = "N") +
+  theme_minimal()  #  cria um gráfico de barras com um tema minimalista (e, portanto, menos distrativo)
+
+# Suponha agora que, em seu projeto de visualização de dados, as informações do eixo Y sejam desnecessárias. Podemos usar nossa função genérica theme para fazer essa edição. Como ja estamos usando um tema, a camada com a função genérica vai logo após o tema em uso.
+
+ggplot(eseb, aes(as.character(D01A_FX_ID))) + 
+  geom_bar() +
+  labs(title = "Número de entrevistados por faixa de idade",
+       subtitle = "ESEB - 2022",
+       x = "Faixa de Idade",
+       y = "N") +
+  theme_classic() +
+  theme(axis.line.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank())
+
+# A função theme é usada para fazer customizações em um gráfico, e a função element_blank() é usada para remover elementos de um gráfico. 
+
+# A função element_line, é usada para customizar linhas de um gráfico
+
+ggplot(eseb, aes(as.character(D01A_FX_ID))) + 
+  geom_bar() +
+  labs(title = "Número de entrevistados por faixa de idade",
+       subtitle = "ESEB 2022",
+       x = "Faixa de Idade",
+       y = "N") +
+  theme_classic() +
+  theme(axis.line.x = element_line(color = "red"))
---
+++ exercicios_cap4/aula_4_exercicios.R
@@ -0,0 +1,91 @@
+#Lista de exercícios do capitulo 4
+#Dan Nogueira
+
+
+######## PACOTES #########
+
+
+
+#########################
+#                       #
+# 1. Gráficos de barras #
+#                       #
+#########################
+
+
+##  Usando a variável Q10P2b, crie um gráfico de barras que contenha o total de votos de cada candidato na amostra do Eseb.
+
+
+# Carregando pacotes
+
+library(haven)
+library(tidyverse)
+
+
+
+# Carregando dados
+
+eseb <- read_sav("eseb2022.sav", encoding = "ASCII")
+eseb <- as_factor(eseb)
+
+
+# Criando o gráfico de barras
+
+ggplot(eseb, aes(x = Q10P2b)) + 
+  geom_bar()
+
+
+
+#####################################
+#                                   #
+# 2. Gráficos de barras com facetas #
+#                                   #
+#####################################
+
+# Crie agora um gráfico de barras que mostra o total de votos de cada candidato na amostra do Eseb, mas com painéis para cada região do país. Use duas colunas para organizar os painéis.
+
+ggplot(eseb, aes(x = Q10P2b)) + 
+  geom_bar() +
+  scale_x_discrete(labels = c("Jair Bolsonaro" = "Bolsonaro")) + 
+  facet_wrap(~ REG, scales = "free_x") +
+  labs(title = "Total de votos por candidato",
+       subtitle = "Eleições 2022",
+       x = "Candidato",
+       y = "Votos") +
+  coord_flip()
+
+
+
+#############################
+#                           #
+# 3. Gráficos de histograma #
+#                           #
+#############################
+
+# carregue novamente dados do Censo de 1872
+censo <- read_delim("https://raw.githubusercontent.com/izabelflores/Censo_1872/main/Censo_1872_dados_tidy_versao2.csv", delim = ";", locale = locale(encoding = "ISO-8859-1"))
+
+#  crie um gráfico que mostre a distribuição das pessoas consideradas de raça preta, segundo a denominação do Censo, indicada pela variável Raças_Preto
+
+ggplot(censo, aes(x = Raças_Preto)) + 
+  geom_histogram() +
+  scale_x_continuous(limits = c(0, 2000))
+
+
+###############################################
+#                                             #
+# 4. Gráficos de barras com totais calculados #
+#                                             #
+###############################################
+
+# crie um gráfico de barras que mostra o total de pessoas por província
+
+pessoas_provincia <- group_by(censo, PrimeiroDeProvincia)
+provincia_resumo <- summarise(pessoas_provincia, pop_provincia = sum(Total_Almas))
+
+ggplot(provincia_resumo, aes(x = PrimeiroDeProvincia, y = pop_provincia)) + 
+  geom_col() +
+  coord_flip()
+
+
+View(provincia_resumo)
