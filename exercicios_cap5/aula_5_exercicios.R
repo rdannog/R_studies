1b9c71582122f0ac13db0e4a2e857f6b2f8347c5
dannog <dandaro.nogueira@gmail.com>
2024-03-20 23:38:10 -0300

    Exercícios, cap 5 - Em andamento

--- exercicios_cap5/aula_5_exercicios.R
+++ exercicios_cap5/aula_5_exercicios.R
@@ -4,11 +4,24 @@
 
 ######## PACOTES #########
 
-
+library(gt)
+library(tidyverse)
+library(modelsummary)
+library(rio)
 
 ###################################
 #                                 #
 # 1. Medidas de tendência central #
 #                                 #
 ###################################
+
+# Carregue o arquivo distribuicao_renda_rfb.txt o salve no objeto renda. 
+
+renda <- import("distribuicao_renda_rfb.txt")
+
+# Em seguida, use operações de manipulação de dados para calcular o total (soma), a média e a mediana da renda tributável reportada (variável rendimentos_tributaveis_milhoes) para cada centil de renda.
+
+
+
+#  O resultado ser uma base nova com uma linha apenas por centil. A variável que indica os centis de renda chama-se centil.
 
