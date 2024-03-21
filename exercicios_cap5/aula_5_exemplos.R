d1b454bb937c0558c6d5ceac2ebe31b7487016ae
dannog <dandaro.nogueira@gmail.com>
2024-03-20 23:19:02 -0300

    Exemplos, Capítulo 5 - Estudos em andamento

--- exercicios_cap5/aula_5_exemplos.R
+++ exercicios_cap5/aula_5_exemplos.R
@@ -63,5 +63,50 @@ tab_longa <- gov |>
             desvio_votos = sd(pct_votos)) |>
   pivot_longer(cols = -uf, names_to = "estatistica", values_to = "valor")
 
+#  Isso feito, podemos usar pivot_wider() para transformar a coluna uf em múltiplas colunas, uma para cada estado:
 
-## 
+tab_final <- tab_longa |>
+  pivot_wider(names_from = uf, values_from = valor)
+
+tab_final
+
+# Nem sempre é intuitivo saber quando usar pivot_longer() e pivot_wider(), e em qual ordem, mas, com um pouco de prática, é fácil pegar os padrões mais recorrentes de transformação de acordo com os princípios tidy
+
+
+## Exportando resultados
+
+# Uma vez calculadas algumas estatísticas descritivas, a sequência natural é exportá-las para um arquivo de texto ou planilha para uso posterior. 
+
+tab_final |>
+  write_csv("minha_tabela.csv")
+
+# O ponto negativo dessa abordagem é que, por padrão, a função write_csv() não aplica nenhuma formatação ao resultado. É por essa razão que sugerimos usar o pacote gt – um pacote que facilita a criação modular de tabelas em HTML, LaTeX ou documentos de texto – para salvar estatísticas descritivas.
+
+tab_final |>
+  gt() |>
+  gtsave("minha_tabela.html")
+
+# A tabela exportada tem uma boa formatação, ainda que falte ajustar detalhes como o excesso de casas decimais e a ausência de título e fonte. Para esses e outros ajustes finos, o pacote gt oferece uma série de funções auxiliares.
+
+tab_final |>
+  gt() |>
+  fmt_number(decimals = 1) |>
+  tab_header(title = "Estatísticas descritivas de gastos e votos por estado") |>
+  tab_source_note(source_note = "Fonte: TSE")
+
+# E, usando um pouco de manipulação de dados, conseguimos renomear a coluna de estatisticas e seus valores para algo mais adequado:
+
+tab_formatada <- tab_final |>
+  rename(Estatística = estatistica) |>
+  mutate(Estatística = case_when(Estatística == "media_gastos" ~ "Média de gastos",
+                                 Estatística == "desvio_gastos" ~ "Desvio de gastos",
+                                 Estatística == "media_votos" ~ "Média de votos",
+                                 Estatística == "desvio_votos" ~ "Desvio de votos")) |>
+  gt() |>
+  fmt_number(decimals = 1) |>
+  tab_header(title = "Estatísticas descritivas de gastos e votos por estado") |>
+  tab_source_note(source_note = "Fonte: TSE.") 
+
+
+tab_formatada |>
+  gtsave("minha_tabela.rtf")
